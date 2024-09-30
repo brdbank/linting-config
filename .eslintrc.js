1cff66858a3374e29267526fa5651cb4bc3ed9ea
Aime Malaika <aimemalaika1995@gmail.com>
2024-09-30 15:56:25 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'import/no-cycle': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
